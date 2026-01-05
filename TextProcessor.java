402d583bb61e09180fb2c843c270861007c1e9a4
Orsun <foralacatag@gmail.com>
2026-01-05 12:42:36 -0500

    Removing elements

---
+++ TextProcessor.java
@@ -0,0 +1,51 @@
+public class TextProcessor {
+    private ArrayList<String> textList;   // The list of text from the file
+    /*
+     * Constructor to create a TextProcessor with the specified filename
+     */
+    public TextProcessor(ArrayList<String> lines) {
+        setTextList(lines);
+    }
+    /*
+    * Changes the textList to the newTextList
+    */
+   public void setTextList(ArrayList<String> newTextList) {
+    textList = newTextList;
+   }
+   
+    /*
+   * Finds and removes all occurrences of each stop word from textList
+   */
+/*
+   * Finds and removes all occurrences of each stop word from textList
+   */
+  public void removeStopWords(ArrayList<String> stopWords) {
+
+  }
+   /*
+   * Returns a String containing the text in textList
+   */
+  public String toString() {
+    String text = "";
+
+    for (String value : textList) {
+      text = text + value + "\n";
+    }
+
+    return text;
+  }
+
+
+
+
+
+
+    public static void main(String[] args) {
+        ArrayList<String> stopwords = FileOperator.getStringList("stopwords.txt");
+        ArrayList<String> posts = FileOperator.getStringList("posts.txt");
+        TextProcessor t = new TextProcessor(posts);
+        t.removeStopWords(stopwords);
+        System.out.println(t);
+    }
+    
+}
