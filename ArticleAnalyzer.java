b40d6d3aaf97717c0d205877f8a31c54c1d08f37
fators22 <116498723+fators22@users.noreply.github.com>
2026-01-16 13:23:00 -0500

    Update ArticleAnalyzer.java

--- ArticleAnalyzer.java
+++ ArticleAnalyzer.java
@@ -22,9 +22,11 @@ public class ArticleAnalyzer {
     public static void main(String[] args) {
        ArticleAnalyzer riano = new ArticleAnalyzer();
        ArrayList<String> lines= FileOperator.getStringList("data.txt");
-       String line =lines.get(0);
+        for(String line : lines){
+    
        Article a=riano.parseJson(line);
        System.out.println(a);
+        }
 
        
 
@@ -44,7 +46,31 @@ public class ArticleAnalyzer {
         Article result;
         Pattern l = Pattern.compile("\"link\":\\s*\"([^\"]+)\"");  //regex to extract words
         Matcher lm =l.matcher(jsonLine); //parameter - line of text
-         String lt = lm.find() ? lm.group(1) : ""; //extract the destined part
+        String lt = lm.find() ? lm.group(1) : ""; //extract the destined part
+
+        
+        Pattern h = Pattern.compile("\"headline\":\\s*\"([^\"]+)\"");  //regex to extract words
+        Matcher hm =l.matcher(jsonLine); //parameter - line of text
+        String ht = lm.find() ? lm.group(1) : ""; //extract the destined part
+        
+        Pattern c = Pattern.compile("\"category\":\\s*\"([^\"]+)\"");  //regex to extract words
+        Matcher cm =l.matcher(jsonLine); //parameter - line of text
+        String ct = lm.find() ? lm.group(1) : ""; //extract the destined part
+
+        Pattern d = Pattern.compile("\"short_description\":\\s*\"([^\"]+)\"");  //regex to extract words
+        Matcher dm =l.matcher(jsonLine); //parameter - line of text
+        String dt = lm.find() ? lm.group(1) : ""; //extract the destined part
+
+
+         Pattern a = Pattern.compile("\"authors\":\\s*\"([^\"]+)\"");  //regex to extract words
+        Matcher am =l.matcher(jsonLine); //parameter - line of text
+        String at = lm.find() ? lm.group(1) : ""; //extract the destined part
+
+         Pattern t = Pattern.compile("\"date\":\\s*\"([^\"]+)\"");  //regex to extract words
+        Matcher tm =l.matcher(jsonLine); //parameter - line of text
+        String tt = lm.find() ? lm.group(1) : ""; //extract the destined part
+
+        
         result=new Article(lt, "", "", "", "","");
 
     return result;
